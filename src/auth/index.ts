7d0d27052d6b26fe63ac8e9df1b72fecca017a86
Warsono <warsono16694@gmail.com>
2021-11-04 23:07:29 +0700

    feat: upgrade vue-auth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -6,7 +6,7 @@ import router from '../router';
 import store from '../store';
 import { authOptions } from './options';
 
-export const useAuth = () => createAuth(authOptions, axios, store, router);
+export const useAuth = () => createAuth(authOptions, store, router, axios);
 
 export const authState = () => {
   const store = useStore();
